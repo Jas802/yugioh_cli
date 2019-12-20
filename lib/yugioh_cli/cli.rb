ed9193ff46c12b72d8f94411f710851375decb22
Jas Spencer <jas.spencer414@gmail.com>
2019-12-20 17:50:10 +0000

    fixed conditional

--- lib/yugioh_cli/cli.rb
+++ lib/yugioh_cli/cli.rb
@@ -10,16 +10,25 @@ class CLI
    display_cards
     puts "Which card number do you want to know more about? Quickly now, we don't have all day!"
     input = gets.strip
+    if input.to_i.between?(0, Card.all.length)
+       card = Card.find(input.to_i)
     
-    card = Card.find(input.to_i)
-    
-    print_card(card)
+      print_card(card)
+      
+  else  
+    puts "Please enter a valid number. It's not that difficult."
+    sleep(3)
+     start
+  
+   
+  
+  end
     
     puts "Would you like to see another amazing card? Simply enter Y or N."
     input = gets.strip.downcase
     if input == "y"
       start
-      elsif input == "n"
+    elsif input == "n"
       puts "I'm sure you were impressed with all you saw! Best of luck, duelist!"
     else
       puts "My advanced brain could not understand that rather simple answer you gave."
